7aedd95d330364842d41fb3a5c89c185b76c92e5
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2020-11-02 07:52:33 +0000

    NearlyDone

---
+++ script.js
@@ -0,0 +1,5 @@
+const wrapper = document.getElementById("rectangleWrapper");
+const rectangles = wrapper.getElementsByClassName("rectangle").innerHTML;
+// const pId = wraper.getElementsByTagName("p").textContent;
+
+console.log("Here are the rectangle IDs: " + rectangles);
